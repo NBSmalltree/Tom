52ee205f6ff13f78a157938d4c60a645492e293e
Tom <tomruibin@163.com>
2018-05-29 15:05:59 +0800

    PSNR_SSIM Amend
    According to Luo Guibo's result

--- YUV_Test/function.cpp
+++ YUV_Test/function.cpp
@@ -16,7 +16,7 @@ double getPSNR(const cv::Mat& I1, const cv::Mat& I2)
 	else
 	{
 		double  mse = sse / (double)(I1.channels() * I1.total());
-		double psnr = 10.0*log10((255 * 255) / mse);
+		double psnr = 10.0*log10((256 * 256) / mse);
 		return psnr;
 	}
 }
@@ -24,6 +24,7 @@ double getPSNR(const cv::Mat& I1, const cv::Mat& I2)
 cv::Scalar getMSSIM(const cv::Mat& i1, const cv::Mat& i2)
 {
 	const double C1 = 6.5025, C2 = 58.5225;
+	int w = 5;
 	/***************************** INITS **********************************/
 	int d = CV_32F;
 
@@ -35,11 +36,11 @@ cv::Scalar getMSSIM(const cv::Mat& i1, const cv::Mat& i2)
 	cv::Mat I1_2 = I1.mul(I1);        // I1^2
 	cv::Mat I1_I2 = I1.mul(I2);        // I1 * I2
 
-								   /***********************初步计算 ******************************/
+	/***********************初步计算 ******************************/
 
 	cv::Mat mu1, mu2;   //
-	cv::GaussianBlur(I1, mu1, cv::Size(11, 11), 1.5);
-	cv::GaussianBlur(I2, mu2, cv::Size(11, 11), 1.5);
+	cv::GaussianBlur(I1, mu1, cv::Size(w, w), 1.5);
+	cv::GaussianBlur(I2, mu2, cv::Size(w, w), 1.5);
 
 	cv::Mat mu1_2 = mu1.mul(mu1);
 	cv::Mat mu2_2 = mu2.mul(mu2);
@@ -47,13 +48,13 @@ cv::Scalar getMSSIM(const cv::Mat& i1, const cv::Mat& i2)
 
 	cv::Mat sigma1_2, sigma2_2, sigma12;
 
-	cv::GaussianBlur(I1_2, sigma1_2, cv::Size(11, 11), 1.5);
+	cv::GaussianBlur(I1_2, sigma1_2, cv::Size(w, w), 1.5);
 	sigma1_2 -= mu1_2;
 
-	cv::GaussianBlur(I2_2, sigma2_2, cv::Size(11, 11), 1.5);
+	cv::GaussianBlur(I2_2, sigma2_2, cv::Size(w, w), 1.5);
 	sigma2_2 -= mu2_2;
 
-	cv::GaussianBlur(I1_I2, sigma12, cv::Size(11, 11), 1.5);
+	cv::GaussianBlur(I1_I2, sigma12, cv::Size(w, w), 1.5);
 	sigma12 -= mu1_mu2;
 
 	///////////////////////////////// 公式 ////////////////////////////////
